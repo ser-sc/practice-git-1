47696d13f1350ef3e844d06a4040346c1385f765
Alexandr Kozhin <alexandr.kozhin@gmail.com>
2019-05-05 22:15:11 +0300

    Add nginx role from galaxy and basic proxypass configuration

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -40,3 +40,17 @@ resource "google_compute_firewall" "firewall_puma" {
   source_ranges = ["0.0.0.0/0"]
   target_tags   = ["reddit-app"]
 }
+
+resource "google_compute_firewall" "firewall_nginx" {
+  name    = "allow-http-default"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+
+    ports = ["80"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["reddit-app"]
+}
