3be018e75ce6ebe592425eed5e2ca7079a471125
ser-sc <sv@schepetin.ru>
2021-01-04 16:52:55 +0300

    +1

--- play-travis/test.py
+++ play-travis/test.py
@@ -4,7 +4,7 @@ class NumbersTest(unittest.TestCase):
 
     def test_equal(self):
         self.assertEqual(1 + 1, 2)
-	print('Hello')
+	print('Hello world')
 
 if __name__ == '__main__':
     unittest.main()
