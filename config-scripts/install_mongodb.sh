21b2a4afadf3fcd5a70a277085176e069c27848c
ser-sc <sv@schepetin.ru>
2021-01-05 21:07:33 +0300

    cloud-testapp

--- config-scripts/install_mongodb.sh
+++ config-scripts/install_mongodb.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv E52529D4
+bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" > /etc/apt/sources.list.d/mongodb-org-4.0.list'
 apt update
 apt install -y mongodb-org
 systemctl start mongod
 systemctl enable mongod
+
+
