18bcaff7056220b9e18267a54b58e363dd75eaff
ser-sc <sv@schepetin.ru>
2021-01-06 17:28:00 +0300

    packer deploy

--- packer/scripts/deploy.sh
+++ packer/scripts/deploy.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
-mv /tmp/puma.service /etc/systemd/system/puma.service
 git clone -b monolith https://github.com/express42/reddit.git
 cd reddit && bundle install
-systemctl enable puma
-systemctl start puma
+puma -d
+ps aux | grep puma
--- packer/scripts/install_mongodb.sh
+++ packer/scripts/install_mongodb.sh
@@ -2,8 +2,14 @@
 set -e
 
 # Install MongoDB
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list
+#apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv E52529D4
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" > /etc/apt/sources.list.d/mongodb-org-4.0.list
+#echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list
 apt-get update
 apt-get install -y mongodb-org
-systemctl enable mongod
+
+sudo systemctl start mongod
+sudo systemctl status mongod 
+
+sudo systemctl enable mongod
